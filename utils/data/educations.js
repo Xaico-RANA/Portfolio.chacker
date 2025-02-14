626a3df1808dbee99c34fd4704bebea8e08089a4
Mohammad RANA <ruhulaminrana98@gmail.com>
2025-02-14 11:33:50 +0600

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -3,14 +3,14 @@ export const educations = [
     id: 1,
     title: "Computational Science and Engineering",
     duration: "2024 - Present",
-    institution: "Georgia Institute of Technology",
+    institution: "Barrister Jamiruddin Sircar Collegiate Institute ,Panchagarh",
   },
 
   {
     id: 2,
-    title: "SSC",
+    title: "Secondary School Certificate",
     duration: "2018 - 2024",
-    institution: "Shalbahan High School Tetulia",
+    institution: "Shalbahan High School Tetulia Panchagarh ",
   },
 
 ]
