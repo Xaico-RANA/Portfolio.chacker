dad4ea545ea2f0d3ffc89df53d2e9eb1f5e67b82
Mohammad RANA <ruhulaminrana98@gmail.com>
2025-02-14 12:15:29 +0600

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Mohammad RANA",
   profile: '/profile.png',
   designation: "Programmer",
-  description: "👋 Hi there! My name is Rana. I'm a double major graduate student at Georgia Tech👩🏻‍💻 I’m a team player with a strong interest in data.I developed my passion for technology when I discovered how big data can transform transit systems.Since then, I've pursued this interest with the help of programs like Rewriting the Code. 👤 Along with my passion for technology, I've developed a strong desire to contribute to social good throughout my life. I enjoy using technology to generate insights that can improve urban mobility, women's rights, equity, and public health.",
+  description: "👋 Hi there ! My name is MOHAMMAD RANA. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'ranaruhulamin80@gmail.com',
   phone: '+8801988686406',
   address: 'Panchagarh Ranpur BD',
@@ -14,5 +14,5 @@ export const personalData = {
   leetcode: "https://bd.linkedin.com/in/mohammad-rana-b007b8325",
   devUsername: "Xaico-RANA",
   // resume: "https://drive.google.com/file/d/1EriWt4yXJT-0YgCELWDEzd7wVP7lNH8X/view?usp=sharing"
-  resume: "https://drive.google.com/file/d/17Iy8-HRmyp-BY53mAz_X3VO686fL7qzm/view?usp=drivesdk"
+  resume: "https://drive.google.com/file/d/1-WqVKqswKNKI2R5gMwf4X3eNjla4XYdi/view?usp=sharing"
 }
