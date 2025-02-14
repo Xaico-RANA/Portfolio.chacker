a9809c0366f1b8f5ca8482decd2f56cd2d43a97c
Mohammad RANA <ruhulaminrana98@gmail.com>
2025-02-14 11:30:12 +0600

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,15 +2,15 @@ export const educations = [
   {
     id: 1,
     title: "Computational Science and Engineering",
-    duration: "2023 - Present",
+    duration: "2024 - Present",
     institution: "Georgia Institute of Technology",
   },
 
   {
     id: 2,
-    title: "Urban Planning",
-    duration: "2018 - 2023",
-    institution: "Beijing Forestry University",
+    title: "SSC",
+    duration: "2018 - 2024",
+    institution: "Shalbahan High School Tetulia",
   },
 
 ]
