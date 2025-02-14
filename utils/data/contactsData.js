25e7479034e1376dfad241fd556c4f96f5144aab
Mohammad RANA <ruhulaminrana98@gmail.com>
2025-02-14 11:28:31 +0600

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -4,8 +4,8 @@ export const contactsData = {
     address: 'Panchagarh, Rangpur, Bangladesh',
     github: 'https://github.com/Xaico-RANA',
     facebook: 'https://facebook.com/XAICO.RANA',
-    linkedIn: 'https://www.linkedin.com/in/tara-tingyu-liu/',
+    linkedIn: 'https://www.linkedin.com/in/mohammad-rana-b007b8325',
     twitter: 'https://twitter.com/drunken-boat',
-    stackOverflow: '',
+    stackOverflow: 'https://stackoverflow.com/users/29369606/mohammad-rana',
     devUsername: "Xaico-RANA"
 }
